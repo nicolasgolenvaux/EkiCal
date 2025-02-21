a6531811cd8a9544169c4748ae4d375c69d53cb5
nicolasgolenvaux <nicolasgolenvaux@gmail.com>
2025-02-21 14:02:25 +0100

    settings Linux updated

--- foundation/Config.php
+++ foundation/Config.php
@@ -1,13 +1,13 @@
 <?php declare(strict_types = 1);
 
 namespace EkiCal\foundation;
-/** Cette classe va nous permettre de récupérer les différentes valeurs de configuration
+/** Cette classe va nous permettre de rÃ©cupÃ©rer les diffÃ©rentes valeurs de configuration
  *qu'on aura dans le dossier /config. Dans le fichier /config, ce ne sont que des tableaux,
- * donc il faudra indiquer la clé pour récupérer la bonne valeur.
+ * donc il faudra indiquer la clÃ© pour rÃ©cupÃ©rer la bonne valeur.
  */
 class Config
 {
-    /** Méthode pour récupérer une valeur de configuration. Pour pouvoir récupérer la clé et le fichier,
+    /** MÃ©thode pour rÃ©cupÃ©rer une valeur de configuration. Pour pouvoir rÃ©cupÃ©rer la clÃ© et le fichier,
      * on va la mettre au format 'file.key'.
      * @param string $config
      * @return mixed
@@ -15,42 +15,43 @@ class Config
 
     public static function get(string $config): mixed
     {
-        // 'File.key' ex : 'app.env'. On utilise la décomposition pour récupérer les valeurs.
+        // 'File.key' ex : 'app.env'. On utilise la dÃ©composition pour rÃ©cupÃ©rer les valeurs.
         [$file, $key] = static::getFileAndKey($config);
-        // On vérifie si le fichier de configuration existe et on récupère son chemin
+        // On vÃ©rifie si le fichier de configuration existe et on rÃ©cupÃ¨re son chemin
         $path = static::getPath($file);
-        // On va affecter le tableau que le fichier retourne à la variable $config
+        // On va affecter le tableau que le fichier retourne Ã  la variable $config
         $config = require $path;
-        // On retourne la valeur de la clé mise en paramètre.
+        // On retourne la valeur de la clÃ© mise en paramÃ¨tre.
         return $config[$key] ?? null;
     }
-    /** Cette fonction retourne un tableau à partir d'une chaîne de caractères avec
-     * un séparateur ('.'). On vérifie si la chaîne a un bon format via un regex.
+    /** Cette fonction retourne un tableau Ã  partir d'une chaÃ®ne de caractÃ¨res avec
+     * un sÃ©parateur ('.'). On vÃ©rifie si la chaÃ®ne a un bon format via un regex.
      * @param string $config
      * @return array
      */
     protected static function getFileAndKey(string $config): array
     {
-        // Si le format est bon, on passe au retour. On utilise une expression régulière.
+        // Si le format est bon, on passe au retour. On utilise une expression rÃ©guliÃ¨re.
         // Voir regex101.com
         if (!preg_match('/^[a-z_]+\.[a-z_]+$/i', $config)) {
             throw new \InvalidArgumentException(
-                sprintf('Mauvais format (%s au lieu de fichier.clé (lettres et _ acceptés))', $config)
+                sprintf('Mauvais format (%s au lieu de fichier.clÃ© (lettres et _ acceptÃ©s))', $config)
             );
         }
-        // On crée un tableau à partir d'une chaîne de caractère
+        // On crÃ©e un tableau Ã  partir d'une chaÃ®ne de caractÃ¨re
         return explode('.', $config);
     }
 
-    /**Cette méthode retourne le chemin d'accès vers le fichier de configuration
-     * On vérifie si le fichier de configuration existe et on récupère son chemin absolu.
+    /**Cette mÃ©thode retourne le chemin d'accÃ¨s vers le fichier de configuration
+     * On vÃ©rifie si le fichier de configuration existe et on rÃ©cupÃ¨re son chemin absolu.
      * @param string $file
      * @return string
      */
     protected static function getPath(string $file): string
     {
-        //Root ramène le chemin de base de mon projet.
-        $path = sprintf('C:/Users/nicol/EkiCal/config/%s.php', $file);
+        //Root ramÃ¨ne le chemin de base de mon projet.
+        $path = sprintf('../config/%s.php', $file);
+
         if (!file_exists($path)) {
             throw new \InvalidArgumentException('Le fichier de configuration n\'existe pas');
         }
--- public/index.php
+++ public/index.php
@@ -1,19 +1,19 @@
 <?php
 declare(strict_types = 1);
-//Ce fichier va récupérer toutes les requêtes qui viennent de l'extérieur.
-//Il va créer l'application et rendre la réponse qu'on attend par rapport
-//à la route demandée par le client et par rapport à la méthode demandée.
+//Ce fichier va rÃ©cupÃ©rer toutes les requÃªtes qui viennent de l'extÃ©rieur.
+//Il va crÃ©er l'application et rendre la rÃ©ponse qu'on attend par rapport
+//Ã  la route demandÃ©e par le client et par rapport Ã  la mÃ©thode demandÃ©e.
 
 
-/** On définit une constante disponible partout dans le projet
-qui va nous permettre d'avoir le répertoire de base du projet
-On remplace le dossier '/public' par rien et on récupère la
- constante __DIR__ (le chemin absolu du répertoire dans lequel on est actuellement).**/
-define('ROOT', str_replace('\public', '', __DIR__));
+/** On dÃ©finit une constante disponible partout dans le projet
+qui va nous permettre d'avoir le rÃ©pertoire de base du projet
+On remplace le dossier '/public' par rien et on rÃ©cupÃ¨re la
+constante __DIR__ (le chemin absolu du rÃ©pertoire dans lequel on est actuellement).**/
+define('ROOT', str_replace(DIRECTORY_SEPARATOR.'public', '', __DIR__));
 
-// On requiert l'auto-loader de composer pour avoir les dépendence partout dans le projet
+// On requiert l'auto-loader de composer pour avoir les dÃ©pendence partout dans le projet
 require_once ROOT.'/vendor/autoload.php';
 
-// On crée une instance de 'App' pour initializer les composants
+// On crÃ©e une instance de 'App' pour initializer les composants
 $app = new EkiCal\foundation\App();
 $app->render();
